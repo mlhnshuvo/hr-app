eaac49817f228a916dc5a458de2f934068f20ce1
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2022-12-09 18:37:38 +0600

    dot menu add

--- src/components/hiringStatus/TitleTop.js
+++ src/components/hiringStatus/TitleTop.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Back from "../../assets/images/back.svg";
 import Bar from "../../assets/images/bar.svg";
 
 const TitleTop = () => {
+  const [menu, setMenu] = useState(false);
+
+  const menuToggle = () => {
+    setMenu(!menu);
+  };
+
   return (
     <div className="tt common-width">
       <div className="tt-back">
@@ -11,7 +17,17 @@ const TitleTop = () => {
         <p className="tt-back-info">M. Tech. (Structure)</p>
         <p className="tt-back-info">+91-9930705653</p>
       </div>
-      <img src={Bar} alt="" />
+      <div className="tt-dot-bar">
+        <img src={Bar} alt="" className="tt-bar" onClick={menuToggle} />
+        {menu && (
+          <ul className="ct-content-hs-items">
+            <li className="ct-content-hs-list">Edit Candidate</li>
+            <li className="ct-content-hs-list">Send Email</li>
+            <li className="ct-content-hs-list">View Past Appliations</li>
+            <li className="ct-content-hs-list">Delete Candidate</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
